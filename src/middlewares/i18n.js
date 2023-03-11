c949edebbcdf16bb1133785881956dab46498e7c
Sergey <klikkn@gmail.com>
2023-03-11 16:17:31 +0100

    fix: intl middleware works for nested fields as qwll

--- src/middlewares/i18n.js
+++ src/middlewares/i18n.js
@@ -2,26 +2,84 @@ module.exports = () => {
   return async (ctx, next) => {
     await next()
     const { locale } = ctx.request.query
-
-    ctx.response.body.data = ctx.response.body.data.map(({ id, attributes }) => {
-      return { id, attributes: translateObject(attributes, locale) }
-    })
+    ctx.response.body.data = translateObject(ctx.response.body.data, locale)
   }
 }
 
-const translateObject = (obj, locale) => {
-  const result = {}
+const translateObject = (data, locale) => {
+  if (Array.isArray(data)) {
+    return data.map((item) => translateObject(item, locale))
+  }
 
-  for (const key in obj) {
-    if (key.includes('_')) {
+  const attributes = {}
+
+  for (const key in data?.attributes) {
+    if (
+      data.attributes[key] &&
+      (Array.isArray(data.attributes[key]) || typeof data.attributes[key] === 'object')
+    ) {
+      attributes[key] = {}
+      attributes[key].data = translateObject(data.attributes[key].data, locale)
+    } else if (key.includes('_')) {
       const [field, fieldLocale] = key.split('_')
-      if (fieldLocale === locale && obj.hasOwnProperty(field)) {
-        result[field] = obj[key]
+      if (fieldLocale === locale && data?.attributes.hasOwnProperty(field)) {
+        attributes[field] = data?.attributes[key]
       }
     } else {
-      result[key] = obj[key]
+      attributes[key] = data?.attributes[key]
     }
   }
 
-  return result
+  return { ...data, attributes }
 }
+
+/**
+ *   "data": [
+    {
+      "id": 1,
+      "attributes": {
+        "title": "ua title",
+        "description": "ua description",
+        "terms": "ua terms",
+        "slug": "promo1",
+        "createdAt": "2023-03-11T12:29:14.212Z",
+        "updatedAt": "2023-03-11T12:29:57.674Z",
+        "publishedAt": "2023-03-11T12:29:57.667Z",
+        "discountTo": 50,
+        "categories": {
+          "data": [
+            {
+              "id": 1,
+              "attributes": {
+                "title": "Beauty",
+                "description": null,
+                "createdAt": "2023-03-11T10:28:39.403Z",
+                "updatedAt": "2023-03-11T10:28:39.403Z",
+                "slug": "beauty",
+                "title_pl": "Uroda",
+                "title_ua": "Краса",
+                "title_ru": "Красота",
+                "description_pl": null,
+                "description_ua": null,
+                "description_ru": null
+              }
+            }
+          ]
+        },
+        "organization": {
+          "data": {
+            "id": 1,
+            "attributes": {
+              "title": "Салон красоты Наталья",
+              "description": "Стрижет как мама",
+              "phone": "3123123123",
+              "createdAt": "2023-03-11T12:28:11.369Z",
+              "updatedAt": "2023-03-11T12:28:11.369Z",
+              "description_pl": "po polsku",
+              "description_ua": "по украiнскi",
+              "description_ru": "по русски"
+            }
+          }
+        }
+      }
+ */
