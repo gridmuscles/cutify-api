4aed20d1f448f9c9a9803c6837622e3f9acffab1
Sergey <klikkn@gmail.com>
2023-03-16 18:59:15 +0100

    feature: firn promotion by slug

--- src/api/promotion/controllers/promotion.js
+++ src/api/promotion/controllers/promotion.js
@@ -122,4 +122,37 @@ module.exports = createCoreController('api::promotion.promotion', ({ strapi }) =
       meta,
     }
   },
+
+  async findOne(ctx) {
+    try {
+      const { locale } = ctx.request.query
+      ctx.request.query = {
+        ...ctx.request.query,
+        filters: {
+          [!locale || locale === 'en' ? 'slug' : `slug_${locale}`]: { $eq: ctx.params.id },
+        },
+      }
+      ctx.params.id = undefined
+
+      const promotions = await this.find(ctx)
+      if (promotions.data.length !== 1) {
+        return
+      }
+
+      await strapi.entityService.update('api::promotion.promotion', promotions.data[0].id, {
+        data: {
+          viewsCount: promotions.data[0].attributes.viewsCount
+            ? promotions.data[0].attributes.viewsCount + 1
+            : 1,
+        },
+      })
+
+      ctx.response.body = {
+        data: promotions.data[0],
+      }
+    } catch (err) {
+      strapi.log.error(err)
+      ctx.badRequest()
+    }
+  },
 }))
