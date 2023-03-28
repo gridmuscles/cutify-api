2f61c8bfdfa5d3d0acd884bbdf9e8a21ada60708
Sergey <klikkn@gmail.com>
2023-03-28 21:39:26 +0200

    feature: extend reservation models

--- src/api/reservation-service/controllers/reservation-service.js
+++ src/api/reservation-service/controllers/reservation-service.js
@@ -3,17 +3,24 @@
 /**
  * reservation-service controller
  */
-
+const { ValidationError } = require('@strapi/utils').errors
 const { createCoreController } = require('@strapi/strapi').factories
+
+const { ERROR_CODES } = require('../../../utils/const')
 
 module.exports = createCoreController(
   'api::reservation-service.reservation-service',
   () => ({
-    async create(ctx) {
-      console.log(ctx.request)
-      console.log(ctx.params)
+    async find(ctx) {
+      const {
+        data: { organization },
+      } = ctx.request.body
 
-      return super.create(ctx)
+      if (!organization) {
+        throw new ValidationError(ERROR_CODES.REQUIRED_FILTERS_ARE_MISSED)
+      }
+
+      return super.find(ctx)
     },
   })
 )
--- src/api/reservation-target/controllers/reservation-target.js
+++ src/api/reservation-target/controllers/reservation-target.js
@@ -3,14 +3,24 @@
 /**
  * reservation-target controller
  */
-
+const { ValidationError } = require('@strapi/utils').errors
 const { createCoreController } = require('@strapi/strapi').factories
+
+const { ERROR_CODES } = require('../../../utils/const')
 
 module.exports = createCoreController(
   'api::reservation-target.reservation-target',
   () => ({
-    async create(ctx) {
-      return super.create(ctx)
+    async find(ctx) {
+      const {
+        data: { organization },
+      } = ctx.request.body
+
+      if (!organization) {
+        throw new ValidationError(ERROR_CODES.REQUIRED_FILTERS_ARE_MISSED)
+      }
+
+      return super.find(ctx)
     },
   })
 )
