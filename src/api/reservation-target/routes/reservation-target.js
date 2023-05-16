ab582f451104089164525440c6a3227db6986901
Sergey <klikkn@gmail.com>
2023-05-16 21:43:18 +0200

    [RESERVATION] Removed because useless

--- src/api/reservation-target/routes/reservation-target.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-/**
- * reservation-target router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories
-
-module.exports = createCoreRouter('api::reservation-target.reservation-target')
--- src/api/reservation-target/services/reservation-target.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-/**
- * reservation-target service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories
-
-module.exports = createCoreService('api::reservation-target.reservation-target')
