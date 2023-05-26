bce530f92eb1981f66e19cc889790de9a605b081
Sergey <klikkn@gmail.com>
2023-05-26 17:18:37 +0200

    fix: articles should be traslated

--- src/api/article/routes/article.js
+++ src/api/article/routes/article.js
@@ -6,4 +6,10 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories
 
-module.exports = createCoreRouter('api::article.article')
+module.exports = createCoreRouter('api::article.article', {
+  config: {
+    findOne: {
+      middlewares: [{ name: 'global::locale' }, { name: 'global::i18n' }],
+    },
+  },
+})
