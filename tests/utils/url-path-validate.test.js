7b4a219fda719ce7dffea7fea37cdad53d95d811
Sergey <klikkn@gmail.com>
2023-06-08 16:40:48 +0200

    feature: url validatipr for future

---
+++ tests/utils/url-path-validate.test.js
@@ -0,0 +1,84 @@
+const { isValidUrlPath } = require('../../src/utils/url-path-validate')
+
+describe('Url validaton', () => {
+  it.each([
+    {
+      url: 'path?param=value',
+      result: true,
+    },
+    {
+      url: 'coupons?filters[uuid][$in][0]=400267069-262509221&filters[uuid][$in][1]=400267069-262509221&filters[promotion][$eq]=12',
+      result: true,
+    },
+    {
+      url: 'example.com',
+      result: false,
+    },
+    {
+      url: 'http://example.com',
+      result: false,
+    },
+    {
+      url: 'https://example.com',
+      result: false,
+    },
+    {
+      url: 'ftp://example.com',
+      result: false,
+    },
+    {
+      url: 'example.com/path?param=value',
+      result: false,
+    },
+    {
+      url: 'http://example.com/path?param=value',
+      result: false,
+    },
+    {
+      url: 'https://example.com/path?param=value',
+      result: false,
+    },
+    {
+      url: 'ftp://example.com/path?param=value',
+      result: false,
+    },
+    {
+      url: 'subdomain.example.com',
+      result: false,
+    },
+    {
+      url: 'http://subdomain.example.com',
+      result: false,
+    },
+    {
+      url: 'https://subdomain.example.com',
+      result: false,
+    },
+    {
+      url: 'ftp://subdomain.example.com',
+      result: false,
+    },
+    {
+      url: 'subdomain.example.com/path?param=value',
+      result: false,
+    },
+    {
+      url: 'http://subdomain.example.com/path?param=value',
+      result: false,
+    },
+    {
+      url: 'https://subdomain.example.com/path?param=value',
+      result: false,
+    },
+    {
+      url: 'ftp://subdomain.example.com/path?param=value',
+      result: false,
+    },
+    {
+      url: 'sub.subdomain.example.com/path?param=value',
+      result: false,
+    },
+  ])('isValidUrlPath $url', ({ url, result }) => {
+    expect(isValidUrlPath(url)).toBe(result)
+  })
+})
