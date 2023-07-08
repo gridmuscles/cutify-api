13fc81a2dbc758d3b9b0e844e627775254b03457
Sergey <klikkn@gmail.com>
2023-07-08 18:08:43 +0200

    fix: location pin generation update

--- src/api/location/content-types/location/lifecycles.js
+++ src/api/location/content-types/location/lifecycles.js
@@ -7,15 +7,17 @@ module.exports = {
     const maxPinLocation = await strapi
       .query('api::location.location')
       .findOne({
-        where: { id: { $not: result.id } },
+        where: {
+          id: { $not: result.id },
+          pin: { $notNull: true },
+          phone: { $notNull: true },
+        },
         orderBy: { pin: 'desc' },
         offset: 0,
         limit: 1,
       })
 
-    const nextPin = maxPinLocation
-      ? (Number(maxPinLocation.pin) + 1).toString().padStart(4, '0')
-      : '0001'
+    const nextPin = (Number(maxPinLocation.pin) + 1).toString().padStart(4, '0')
 
     await strapi
       .query('api::location.location')
