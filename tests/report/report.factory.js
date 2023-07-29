5db53315e8e5ae437b9b40c7148e7f3e532501b6
Sergey <klikkn@gmail.com>
2023-07-29 17:03:56 +0200

    feat(report): coupons connection

--- tests/report/report.factory.js
+++ tests/report/report.factory.js
@@ -11,6 +11,14 @@ const createReport = async (data = {}) => {
   })
 }
 
+const getReportById = async (id) => {
+  return strapi.db.query('api::report.report').findOne({
+    where: { id },
+    populate: true,
+  })
+}
+
 module.exports = {
   createReport,
+  getReportById,
 }
